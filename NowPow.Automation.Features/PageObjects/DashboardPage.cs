8d7490cc04653455e4ff14e9161012bb165522c8
Stephanie Hasz <stephanie.hasz@nowpow.com>
2019-10-17 14:43:32 -0500

    Updating URL and usernames for dev.

--- NowPow.Automation.Features/PageObjects/DashboardPage.cs
+++ NowPow.Automation.Features/PageObjects/DashboardPage.cs
@@ -39,12 +39,14 @@ namespace NowPow.Automation.Features.StepDefinitions
             patientCard.Click();
             return this;
         }
+
         //Open subtab "Needs" from patient details
         internal DashboardPage OpenNeeds(string subtabName)
         {
             S(By.Id(subtabName.ToLower())).Click();
             return this;
         }
+
         //To add needs modal
         internal AddNeedModal AddNeed()
         {
@@ -52,6 +54,7 @@ namespace NowPow.Automation.Features.StepDefinitions
             WaitFor(addNeedButton, Be.Visible).Click();
             return new AddNeedModal(DriverContext);
         }
+
         //To open "Take Action" dropdown menu
         internal DashboardPage TakeAction()
         {
@@ -64,6 +67,7 @@ namespace NowPow.Automation.Features.StepDefinitions
             addInteraction.Click();
             return new AddInteractionModal(DriverContext);
         }
+
         internal DashboardPage OpenDrawer()
         {
             WaitForNot(S("#patientInteractionModal-container"), Be.InDom);
@@ -74,18 +78,25 @@ namespace NowPow.Automation.Features.StepDefinitions
         //Verify Patient tab
         internal PatientPage OpenPatient(string tabName)
         {
-            
+            WaitFor(hamburgerIcon, Be.Visible);
+            hamburgerIcon.Click();
+            WaitFor(patientTab, Be.Visible);
             patientTab.Click();
             WaitFor(S("#count-top"), Be.Visible);
             return new PatientPage(DriverContext);
         }
+
         //Verify Referrals tab
         internal ReferralsSentPage OpenReferrals(string tabName)
         {
+            WaitFor(hamburgerIcon, Be.Visible);
+            hamburgerIcon.Click();
+            WaitFor(referralsTab, Be.Visible);
             referralsTab.Click();
             WaitFor(S("#main-header>div>h2"), Be.Visible);
             return new ReferralsSentPage(DriverContext);
         }
+
         //Verify Screening Tab
         internal ScreeningPage OpenScreenings(string tabName)
         {
@@ -93,12 +104,14 @@ namespace NowPow.Automation.Features.StepDefinitions
             WaitFor(S(".col-xs-12.row.card-white > div:nth-child(1)"), Be.Visible);
             return new ScreeningPage(DriverContext);
         }
+
         //Verify eRx tab
         internal ErxPage OpenErx(string tabName)
         {
             erxTab.Click();
             return new ErxPage(DriverContext);
         }
+
         //Verify Services tab
         internal ServicePage OpenServices(string tabName)
         {
