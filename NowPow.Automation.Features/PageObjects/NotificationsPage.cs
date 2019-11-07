c96c00921fd920879b0b67c57263edb4bb767633
Sabina Dovlati <sabina.dovlati@nowpow.com>
2019-11-07 16:29:32 -0600

    added Notifications Page

---
+++ NowPow.Automation.Features/PageObjects/NotificationsPage.cs
@@ -0,0 +1,27 @@
+﻿using Ocaramba;
+using static NSelene.Selene;
+using OpenQA.Selenium;
+using NSelene;
+using NowPow.Automation.PageObjects;
+using NLog;
+using System;
+
+namespace Nowpow.Automation.Features.StepDefinitions
+{
+    internal class NotificationsPage : ProjectPageBase
+    {
+        private static readonly NLog.Logger Logger = LogManager.GetCurrentClassLogger();
+        SeleneElement entryContainer = S("div.entry-container");
+        public NotificationsPage(DriverContext driverContext) : base(driverContext)
+        {
+            
+        }
+
+        internal ReferralsReceivedPage ChooseUnreadNotification()
+        {
+            WaitForNot(S("div.overlay"), Be.InDom);
+            entryContainer.Click();
+            return new ReferralsReceivedPage(DriverContext);
+        }
+    }
+}
